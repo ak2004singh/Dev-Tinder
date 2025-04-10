d8b295f4be278037bb4016cdb60393c671b5e87f
ak2004-star <adityasingh67162@gmail.com>
2025-04-10 18:44:56 +0530

    created get , post , patch , delete api on my own and added validators in the database  , required fields

--- src/app.js
+++ src/app.js
@@ -5,25 +5,76 @@ const app = express();
 app.use(express.json());
 app.get("/user",async(req,res)=>{
     try{
-        const user = await User.find({email:req.body.email});
-        res.send(user);
+        const user = await User.find({email:req.body.email,password:req.body.password});
+        if(user.length === 0){
+            res.status(404).send("User not found");
+            console.log("User not found");
+        }
+        else 
+        {res.send(user);
+        console.log("User fetched successfully");}
     }
     catch(err){
         res.status(404).send("Error in fetching users");
         console.log(err);
     }
-})
+});
 app.post("/signup",async (req,res)=>{
    
-    const user = new User(req.body);
-    await user.save().
-    then(()=>{
-        res.send("Account Created Successfully");
-    })
-    .catch((err)=>{
-        res.status(400).send("Error in creating account");
+    try{
+        const user = new User(req.body);
+        await user.save();
+        res.send("Congratulations! You have signed up successfully");
+        console.log("User signed up successfully");
+    }
+    catch(err){
+        res.status(400).send("Error in signing up user");
         console.log(err);
-    });
+    }
+});
+app.patch("/user",async(req,res)=>{
+    const updatData = req.body;
+    const userId = req.body._id;
+    try{
+        const updatedUser = await User.findByIdAndUpdate(userId,updatData,{
+            new:true,
+            runValidators:true
+        });
+        if(!updatedUser)
+        {
+            res.status(404).send("User not found");
+            console.log("User not found");
+        }
+        else 
+        {
+            res.send("User  Data updated successfully");
+            console.log("User data updated successfully",updatedUser);
+        }
+    }
+    catch(err){
+        res.status(400).send("Error in updating user data");
+        console.log(err);
+    }
+});
+app.delete("/user",async(req,res)=>{
+    const userId = req.body._id;
+    try{
+        const deletedUser = await User.findByIdAndDelete(userId);
+        if(!deletedUser){
+            res.status(404).send("User not found");
+            console.log("User not found");
+        }
+        else 
+        {
+            res.send("User deleted successfully");
+            console.log("User deleted successfully");
+        }
+        
+    }
+    catch(err){
+        res.status(400).send("Error in deleting user data");
+        console.log(err);
+    }
 });
 connectDB().
 then(()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -1,26 +1,100 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        trim:true,
+        minLength:3,maxlength:20,
+        validate(value){
+            if(!value.match(/^[A-Za-z]+$/)){
+                throw new Error("First name should only contain alphabets")
+            }
+        }
     },
     lastName:{
-        type:String
+        type:String,
+        required:true,
+        trim:true,
+        minLength:3,maxlength:20,
+        validate(value){
+            if(!value.match(/^[A-Za-z]+$/)){
+                throw new Error("Last name should only contain alphabets")
+            }
+        }
     },
     email:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        trim:true,
+        validate(value){
+            if (!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(value)) {
+                throw new Error("Email is not valid");}
+        }
     },
     password:{
         type:String,
-        required:true
-    },
-    age:{
-        type:Number
+        required:true,
+        trim:true,
+        minLength:6,maxlength:20,
+        validate(value){
+            if(!value.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{6,20}$/)){
+                throw new Error("Password should contain at least one uppercase letter, one lowercase letter, one number, and be between 6 to 20 characters long");
+            }
+        }
     },
     gender:{
-        type:String
+        type:String,
+        required:true,
+        validate(value){
+            value = value.trim();
+            value = value.toLowerCase();
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Invalid Gender ");
+            }
+        }
+    },
+    age:{
+        type:Number,
+        required:true,
+        min:4,
+        max:100
+    },
+    phone:{
+        type:String,
+        validate(value){
+            if(!value.match(/^[0-9]{10}$/)){
+                throw new Error("Phone number is not valid")
+            }
+        }
+    },
+    bio:{
+        type:String,
+        trim:true,
+        maxLength:100
+    },
+    image:{
+        type:String,
+        default:"https://www.hindustantimes.com/ht-img/img/2025/03/26/550x309/IMG_9850_1743016356335_1743016432763.jpeg",
+        trim:true
+    },
+    skills:{
+        type:[String],
+        required:true,
+        validate(value) {
+            if (!value.every((skill) => /^[A-Za-z0-9+]+$/.test(skill))) {
+              throw new Error("Skills can only contain letters, numbers, and '+'");
+            }
+          }
+    },
+    location:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    project1:{
+        type:String,
+        trim:true
     }
-
-});
-module.exports = mongoose.model('User', userSchema);
+},{timestamps:true});
+module.exports = mongoose.model("User",userSchema);
